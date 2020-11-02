eae750081fb2d1645329bfe2b3a3604158ea837f
Sunbin <shen1qing9@163.com>
2020-11-02 07:54:20 +0000

    shang

--- app/Http/Controllers/SessionsController.php
+++ app/Http/Controllers/SessionsController.php
@@ -17,7 +17,7 @@ class SessionsController extends Controller
         return view('sessions.create');
     }
 
- public function store(Request $request)
+  public function store(Request $request)
     {
        $credentials = $this->validate($request, [
            'email' => 'required|email|max:255',
--- routes/web.php
+++ routes/web.php
@@ -6,10 +6,9 @@ Route::get('/about', 'StaticPagesController@about')->name('about');
 
 Route::get('signup', 'UsersController@create')->name('signup');
 Route::resource('users', 'UsersController');
+
 Route::get('login', 'SessionsController@create')->name('login');
 Route::post('login', 'SessionsController@store')->name('login');
 Route::delete('logout', 'SessionsController@destroy')->name('logout');
-Route::get('password/reset', 'Auth\ForgotPasswordController@showLinkRequestForm')->name('password.request');
-Route::post('password/email', 'Auth\ForgotPasswordController@sendResetLinkEmail')->name('password.email');
-Route::get('password/reset/{token}', 'Auth\ResetPasswordController@showResetForm')->name('password.reset');
-Route::post('password/reset', 'Auth\ResetPasswordController@reset')->name('password.update');
+
+Route::get('signup/confirm/{token}', 'UsersController@confirmEmail')->name('confirm_email');
